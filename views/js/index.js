b15bf805a60de1636ac4591c24e4b5e465312b22
Cyber Witch <no-reply>
2019-05-20 21:47:07 -0600

    created buttons to create a new row with a form and inputs and delete the row

--- views/js/index.js
+++ views/js/index.js
@@ -1,23 +1,64 @@
 
 //bill tracking page.
-
+var nextRowId = 0;
 
 //AJAX get request to select all bills from a certain user goes here.
 //Puts response from ajax into table rows.  Should go in .then function.
 for(let i = 0; i < 4; i++){
     $("#bill-table").append(`
     <tr data-id = "${i}" class = "bill-row">
-    <th>Ajax response goes here</th>
-    <th>Response goes here</th>
-    <th position= "relative">Response
-        <img class = "edit-button" data-id = "${i}" src="assets/images/edit.png" width= "18px">
-        <img class = "delete-button" data-id = "${i}" src="assets/images/delete.png" width= "16px">
-    </th>
-</tr>`)
+        <th>Ajax response goes here</th>
+        <th>Response goes here</th>
+        <th position= "relative">Response
+            <img class = "edit-button" data-id = "${i}" src="assets/images/edit.png" width= "18px">
+            <img class = "delete-button" data-id = "${i}" src="assets/images/delete.png" width= "16px">
+        </th>
+    </tr>`)
+
+    nextRowId++
 }
 
+//Clicking on the new-bill-button will add a form to the bottom of the table.
+$("body").on("click","#new-bill-button",function(){
+    $("#bill-table").append(`
+    <tr id="new-bill-row">
+        <th>
+            <form id = "bill-form">
+                <input name = "bill-name">
+            </form>
+        </th>
+        <th>
+            <input form = "bill-form" name = "amount">
+        </th>
+        <th>
+            <input form = "bill-form" name = "due">
+        </th>
+    </tr>`)
+
+    $("#button-flex").html(`
+    <div id = "cancel-button" class = "button">
+        <img src = "assets/images/delete.png" width = "18px">
+    </div>
+    <div id = "submit-button" class = "button">
+        <img src = "assets/images/checkmark.png" width = "18px">
+    </div>`)
+
+    $("#button-flex").css("justify-content","space-between")
+})
+
+// Clicking the cancel button removes the form from the bottom of the table
+$("body").on("click","#cancel-button",function(){
+    $("#new-bill-row").remove();
+    
+    $("#button-flex").html(`
+    <div id = "new-bill-button" class = "button">+</div>`)
+
+    $("#button-flex").css("justify-content","right")
+})
+
+
 //shows edit and delete button where hovering over row and hides them when exiting.
-$(".bill-row").mouseenter(function(){
+$("body").on("mouseenter",".bill-row",function(){
     let id = $(this).data("id")
     $(".edit-button").each(function(){
         if($(this).data("id") === id){
@@ -33,7 +74,7 @@ $(".bill-row").mouseenter(function(){
 
 })
  
-$(".bill-row").mouseleave(function(){
+$("body").on("mouseleave",".bill-row",function(){
     let id = $(this).data("id")
     $(".edit-button").each(function(){
         if($(this).data("id") === id){
@@ -46,30 +87,31 @@ $(".bill-row").mouseleave(function(){
             $(this).css("display","none")
         }
     })
-
+    
 })
  
 //adjusts opacity when user hovers over or leaves buttons.
-$(".edit-button").mouseenter(function(){
+$("body").on("mouseenter",".edit-button",function(){
     $(this).css("opacity","1")
 })
 
-$(".edit-button").mouseleave(function(){
+$("body").on("mouseleave",".edit-button",function(){
     $(this).css("opacity",".3")
 })
  
-$(".delete-button").mouseenter(function(){
+$("body").on("mouseenter",".delete-button",function(){
     $(this).css("opacity","1")
 })
 
-$(".delete-button").mouseleave(function(){
+$("body").on("mouseleave",".delete-button",function(){
     $(this).css("opacity",".3")
 })
  
-$("#new-bill-button").mouseenter(function(){
+$("body").on("mouseenter",".button",function(){
     $(this).css("opacity","1")
 })
  
-$("#new-bill-button").mouseleave(function(){
+$("body").on("mouseleave",".button",function(){
     $(this).css("opacity",".5")
 })
+
